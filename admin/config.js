328171996b77bc76988d29c600bcddee6f42affc
Kishan S T <stkishan45@gmail.com>
2026-04-09 23:54:43 +0530

    Update Supabase client keys to provided publishable token

--- admin/config.js
+++ admin/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://xixrlokrxylsxspqjpvtq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpeHJsb2tyeHlseHNwcWpwdnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NTA1MTksImV4cCI6MjA5MTMyNjUxOX0.alSE8QT_npouUv_qX61yKjO-zyrh2gX6W30dYaJXiu8';
+const SUPABASE_ANON_KEY = 'sb_publishable_tLfX5EE5rjYxAeO0b9X4nw_9LF2iSVg';
 
 window.SUPABASE_URL = SUPABASE_URL;
 window.SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
--- js/public-config.js
+++ js/public-config.js
@@ -1,3 +1,3 @@
 const PUBLIC_SUPABASE_URL = 'https://xixrlokrxylsxspqjpvtq.supabase.co';
-const PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpeHJsb2tyeHlseHNwcWpwdnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NTA1MTksImV4cCI6MjA5MTMyNjUxOX0.alSE8QT_npouUv_qX61yKjO-zyrh2gX6W30dYaJXiu8';
+const PUBLIC_SUPABASE_ANON_KEY = 'sb_publishable_tLfX5EE5rjYxAeO0b9X4nw_9LF2iSVg';
 window.supabaseClientPublic = window.supabase.createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
