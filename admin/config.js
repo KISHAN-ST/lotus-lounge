2f4491bf9e5d6fc11d14007fffadb9dbd9a2487a
Kishan S T <stkishan45@gmail.com>
2026-04-10 00:07:25 +0530

    Fix Supabase project URL typo in config

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://xixrlokrxylsxspqjpvtq.supabase.co';
+const SUPABASE_URL = 'https://xixrlokrxylxspqjpvtq.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_tLfX5EE5rjYxAeO0b9X4nw_9LF2iSVg';
 
 window.SUPABASE_URL = SUPABASE_URL;
--- js/public-config.js
+++ js/public-config.js
@@ -1,3 +1,3 @@
-const PUBLIC_SUPABASE_URL = 'https://xixrlokrxylsxspqjpvtq.supabase.co';
+const PUBLIC_SUPABASE_URL = 'https://xixrlokrxylxspqjpvtq.supabase.co';
 const PUBLIC_SUPABASE_ANON_KEY = 'sb_publishable_tLfX5EE5rjYxAeO0b9X4nw_9LF2iSVg';
 window.supabaseClientPublic = window.supabase.createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
